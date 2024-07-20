bf652361f965eef3403f37694f15cb1b57213fb2
pzambao <pedrozambaoo@gmail.com>
2024-07-20 20:21:33 -0300

    adicao do useForm e SubmitHandler do react hook form

--- src/pages/search_cep.tsx
+++ src/pages/search_cep.tsx
@@ -1,49 +1,44 @@
-import Head from "next/head"
-import { FormEvent, useEffect, useState } from "react";
-import axios from "axios";
+import Head from 'next/head';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import axios from 'axios';
+import { useEffect } from 'react';
+
+interface CepValues {
+  cep: string;
+  uf: string;
+  localidade: string;
+  bairro: string;
+  logradouro: string;
+  number: string;
+  complement: string;
+}
 
 const SearchCep = () => {
-  const [cep, setCep] = useState('');
-  const [uf, setUf] = useState('');
-  const [localidade, setCity] = useState('');
-  const [bairro, setDistrict] = useState('');
-  const [logradouro, setStreet] = useState('');
-  const [number, setNumber] = useState('');
-  const [complement, setComplement] = useState('');
+  const { register, handleSubmit, setValue, watch, formState: { errors } } = useForm<CepValues>();
+  const cepValue = watch('cep');
 
   const fetchCepData = async (cep: string) => {
     try {
       const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
       const data = response.data;
-      setUf(data.uf);
-      setCity(data.localidade);
-      setDistrict(data.bairro);
-      setStreet(data.logradouro);
+      setValue('uf', data.uf);
+      setValue('localidade', data.localidade);
+      setValue('bairro', data.bairro);
+      setValue('logradouro', data.logradouro);
     } catch (error) {
       console.log('Erro ao buscar o CEP:', error);
     }
   };
 
   useEffect(() => {
-    if (cep.length === 8) {
-      fetchCepData(cep);
+    if (cepValue?.length === 8) {
+      fetchCepData(cepValue);
     }
-  }, [cep]);
+  }, [cepValue]);
 
-  const handleSubmit = (e: FormEvent) => {
-    e.preventDefault();
-
-    const data = {
-      cep,
-      uf,
-      localidade,
-      bairro,
-      number,
-      complement
-    }
-
-    console.log(data)
-  }
+  const onSubmit: SubmitHandler<CepValues> = data => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -51,40 +46,74 @@ const SearchCep = () => {
         <title>Buscar CEP | NextJS Form</title>
       </Head>
       <main className='flex items-center justify-center p-8'>
-        <form className='h-120 w-128 p-8 bg-gray-900 text-white rounded-lg' onSubmit={handleSubmit}>
+        <form className='h-120 w-128 p-8 bg-gray-900 text-white rounded-lg' onSubmit={handleSubmit(onSubmit)}>
           <div className='mb-6'>
             <label htmlFor='cep' className='block text-sm font-medium text-left mb-1'>CEP</label>
-            <input type='text' name='cep' onChange={(e) => setCep(e.target.value)} className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' />
+            <input 
+              type='text' 
+              {...register('cep', { required: true, minLength: 8, maxLength: 8 })} 
+              className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' 
+            />
+            {errors.cep && <span>CEP é obrigatório e deve ter 8 caracteres</span>}
           </div>
 
           <div className='mb-6'>
             <label htmlFor='uf' className='block text-sm font-medium text-left mb-1'>Estado</label>
-            <input type='text' value={uf} name='uf' onChange={(e) => setUf(e.target.value)} className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' />
+            <input 
+              type='text' 
+              {...register('uf', { required: true })} 
+              className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' 
+            />
+            {errors.uf && <span>Estado é obrigatório</span>}
           </div>
 
           <div className='mb-6'>
             <label htmlFor='localidade' className='block text-sm font-medium text-left mb-1'>Cidade</label>
-            <input type='text' value={localidade} name='localidade' onChange={(e) => setCity(e.target.value)} className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' />
+            <input 
+              type='text' 
+              {...register('localidade', { required: true })} 
+              className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' 
+            />
+            {errors.localidade && <span>Cidade é obrigatória</span>}
           </div>
 
           <div className='mb-6'>
             <label htmlFor='bairro' className='block text-sm font-medium text-left mb-1'>Bairro</label>
-            <input type='text' value={bairro} name='bairro' onChange={(e) => setDistrict(e.target.value)} className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' />
+            <input 
+              type='text' 
+              {...register('bairro', { required: true })} 
+              className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' 
+            />
+            {errors.bairro && <span>Bairro é obrigatório</span>}
           </div>
 
           <div className='mb-6'>
             <label htmlFor='logradouro' className='block text-sm font-medium text-left mb-1'>Rua</label>
-            <input type='text' value={logradouro} name='logradouro' onChange={(e) => setStreet(e.target.value)} className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' />
+            <input 
+              type='text' 
+              {...register('logradouro', { required: true })} 
+              className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' 
+            />
+            {errors.logradouro && <span>Rua é obrigatória</span>}
           </div>
 
           <div className='mb-6'>
             <label htmlFor='number' className='block text-sm font-medium text-left mb-1'>Número</label>
-            <input type='text' name='number' className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' />
+            <input 
+              type='text' 
+              {...register('number', { required: true })} 
+              className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white'
+            />
+            {errors.number && <span>Número é obrigatório</span>}
           </div>
 
           <div className='mb-6'>
             <label htmlFor='complement' className='block text-sm font-medium text-left mb-1'>Complemento</label>
-            <input type='text' name='complement' className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' />
+            <input 
+              type='text' 
+              {...register('complement')} 
+              className='w-full bg-transparent border-b border-white focus:outline-none focus:border-white text-white' 
+            />
           </div>
 
           <div className='text-right'>
@@ -93,7 +122,7 @@ const SearchCep = () => {
         </form>
       </main>
     </>
-  )
-}
+  );
+};
 
 export default SearchCep;
